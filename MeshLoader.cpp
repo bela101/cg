e42c52b519360ee039d0621d2836d87f762a6ebf
Goncalo Cardoso <goncalo.cardoso71@gmail.com>
2024-06-25 00:17:09 +0200

    Code cleanup

--- MeshLoader.cpp
+++ MeshLoader.cpp
@@ -6,27 +6,11 @@
 #define NUM_OF_TILES 16
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 
-// The uniform buffer objects data structures
-// Remember to use the correct alignas(...) value
-//        float : alignas(4)g
-//        vec2  : alignas(8)
-//        vec3  : alignas(16)
-//        vec4  : alignas(16)
-//        mat3  : alignas(16)
-//        mat4  : alignas(16)
-// Example:
 struct UniformBlock
 {
 	alignas(16) glm::mat4 mvpMat;
 };
-// struct GlobalUniformBlock{
-// 	alignas(16) glm::vec3 DirectLightColor;
-// 	alignas(16) glm::vec3 DirectLightPos;
-// 	alignas(16) glm::vec3 AmbientLightColor;
-// 	alignas(16) glm::vec3 CameraPos;
-// };
-// struct spotLightObject{
-// };
+
 struct UniformBufferObject
 {
 	alignas(16) glm::mat4 mvpMat;
@@ -82,7 +66,7 @@ protected:
 	// Current aspect ratio (used by the callback that resized the window
 	float Ar;
 	int initialized = 0;
-	// 
+	
 	Car car;
 	Tile tile[NUM_OF_TILES];
 	Tile beach[NUM_OF_TILES * 2];
@@ -120,7 +104,7 @@ protected:
 	Model<Vertex> MSplash2;
 
 	// Descriptor sets
-	DescriptorSet DS1, DS2, DS3; // DS4;
+	DescriptorSet DS1, DS2, DS3; 
 	DescriptorSet DSTiles[NUM_OF_TILES];
 	DescriptorSet DSBeach[NUM_OF_TILES * 2];
 	DescriptorSet DSObstacles[NUM_OF_TILES];
@@ -131,7 +115,7 @@ protected:
 	DescriptorSet DSSplash2;
 
 	// Textures
-	Texture /*T1,*/ T2;
+	Texture T2;
 	Texture TDungeon;
 	Texture TOcean;
 	Texture TSand;
@@ -143,10 +127,9 @@ protected:
 	TextMaker score;
 
 	// C++ storage for uniform variables
-	UniformBlock ubo1, ubo2, ubo3, uboSplash1, uboSplash2; // ubo4;
+	UniformBlock ubo1, ubo2, ubo3, uboSplash1, uboSplash2; 
 	UniformBufferObject ubo;
 	GlobalUniformBufferObject gubo;
-	// spotLightObject slo;
 
 	// Other application parameters
 	std::vector<SingleText> texts = {};
@@ -163,14 +146,13 @@ protected:
 		initialBackgroundColor = { 0.0f, 0.005f, 0.01f, 1.0f };
 
 		// Descriptor pool sizes
-		uniformBlocksInPool = 200;
-		texturesInPool = 200;
-		setsInPool = 200;
+		uniformBlocksInPool = 168;
+		texturesInPool = 87;
+		setsInPool = 87;
 
 		Ar = (float)windowWidth / (float)windowHeight;
 	}
 
-	// What to do when the window changes size - don't remove this for now 
 	void onWindowResize(int w, int h) {
 		Ar = (float)w / (float)h;
 	}
@@ -180,13 +162,7 @@ protected:
 	void localInit()
 	{
 		// Descriptor Layouts [what will be passed to the shaders]
-		DSL.init(this, {// this array contains the bindings:
-						// first  element : the binding number
-						// second element : the type of element (buffer or texture)
-						//                  using the corresponding Vulkan constant
-						// third  element : the pipeline stage where it will be used
-						//                  using the corresponding Vulkan constant
-						{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS}, // DSL DS's have one Uniform Buffer
+		DSL.init(this, {{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS}, // DSL DS's have one Uniform Buffer
 						{1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT}}); // DSL DS's have one texture
 
 		DSLBlinn.init(this, {{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS}, // DSLBLINN DS's have two Uniform Buffers
@@ -197,7 +173,6 @@ protected:
 		DSLCookTorrance.init(this, {{0, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS}, // DSLCookTorrance DS's have two Uniform Buffers
 									{1, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER, VK_SHADER_STAGE_FRAGMENT_BIT}, // DSLCookTorrance DS's have one Texture
 									{2, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS},
-									// {3, VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER, VK_SHADER_STAGE_ALL_GRAPHICS},
 						});
 
 
@@ -214,41 +189,14 @@ protected:
 						});
 		
 		// Vertex descriptors
-		VD.init(this, {// this array contains the bindings
-					   // first  element : the binding number
-					   // second element : the stride of this binging
-					   // third  element : whether this parameter change per vertex or per instance
-					   //                  using the corresponding Vulkan constant
-					   {0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX}},
+		VD.init(this, {
+				{0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX}},
 				{{0, 0, VK_FORMAT_R32G32B32_SFLOAT, offsetof(Vertex, pos), sizeof(glm::vec3), POSITION},
 				 {0, 1, VK_FORMAT_R32G32_SFLOAT, offsetof(Vertex, UV), sizeof(glm::vec2), UV},
-				// this array contains the location
-				 // first  element : the binding number
-				 // second element : the location number
-				 // third  element : the offset of this element in the memory record
-				 // fourth element : the data type of the element
-				 //                  using the corresponding Vulkan constant
-				 // fifth  elmenet : the size in byte of the element
-				 // sixth  element : a constant defining the element usage
-				 //                   POSITION - a vec3 with the position
-				 //                   NORMAL   - a vec3 with the normal vector
-				 //                   UV       - a vec2 with a UV coordinate
-				 //                   COLOR    - a vec4 with a RGBA color
-				 //                   TANGENT  - a vec4 with the tangent vector
-				 //                   OTHER    - anything else
-				 //
-				 // ***************** DOUBLE CHECK ********************
-				 //    That the Vertex data structure you use in the "offsetoff" and
-				 //	in the "sizeof" in the previous array, refers to the correct one,
-				 //	if you have more than one vertex format!
-				 // ***************************************************
 				 });
 
 		// Pipelines [Shader couples]
-		// The second parameter is the pointer to the vertex definition
-		// Third and fourth parameters are respectively the vertex and fragment shaders
-		// The last array, is a vector of pointer to the layouts of the sets that will
-		// be used in this pipeline. The first element will be set 0, and so on..
+
 		P.init(this, &VD, "shaders/ShaderVert.spv", "shaders/ShaderFrag.spv", { &DSL });
 		P.setAdvancedFeatures(VK_COMPARE_OP_LESS_OR_EQUAL, VK_POLYGON_MODE_FILL,
 							  VK_CULL_MODE_NONE, false);
@@ -264,10 +212,7 @@ protected:
 		// Models, textures and Descriptors (values assigned to the uniforms)
 
 		// Create models
-		// The second parameter is the pointer to the vertex definition for this model
-		// The third parameter is the file name
-		// The last is a constant specifying the file type: currently only OBJ or GLTF
-		// MCar.init(this, &VD, "models/transport_sport_001_transport_sport_001.001.mgcg", MGCG);
+
 		MCar.init(this, &VDBlinn, "models/transport_sport_001_transport_sport_001.001.mgcg", MGCG);
 		MSplash1.init(this, &VDBlinn, "models/Square.obj", OBJ);
 		MSplash2.init(this, &VDBlinn, "models/Square.obj", OBJ);
@@ -369,7 +314,6 @@ protected:
 		gubo.spotLightDir = glm::normalize(glm::vec3(0.0f, 0.0f, 1.0f));
 		scene.gameState = 0;
 		scene.text = 0;
-        //texts.push_back({1, {"Infinite Run - Press Space to Start", "", "", ""}, 0, 0});
         texts.push_back({1, {"Infinite Run", "", "", ""}, 0, 0});
 
 		texts[0].l[1] = (char *)malloc(200);
@@ -392,12 +336,6 @@ protected:
 		// Here you define the data set
 
 		DS1.init(this, &DSL, {
-			// the second parameter, is a pointer to the Uniform Set Layout of this set
-			// the last parameter is an array, with one element per binding of the set.
-			// first  elmenet : the binding number
-			// second element : UNIFORM or TEXTURE (an enum) depending on the type
-			// third  element : only for UNIFORMs, the size of the corresponding C++ object. For texture, just put 0
-			// fourth element : only for TEXTUREs, the pointer to the corresponding texture object. For uniforms, use nullptr
 			{0, UNIFORM, sizeof(UniformBlock), nullptr},
 			{1, TEXTURE, 0, &T2} });
 
@@ -486,8 +424,6 @@ protected:
 		DSSplash1.cleanup();
 		DSSplash2.cleanup();
 
-		// DS4.cleanup();
-
 		for (DescriptorSet &DSTiles: DSTiles) {
             DSTiles.cleanup();
         }
@@ -514,7 +450,6 @@ protected:
 	void localCleanup()
 	{
 		// Cleanup textures
-		// T1.cleanup();
 		T2.cleanup();
 		TDungeon.cleanup();
 		TOcean.cleanup();
@@ -529,7 +464,6 @@ protected:
 		MSplash1.cleanup();
 		MSplash2.cleanup();
 
-		// M4.cleanup();
 		for (Model<Vertex> &MTiles: MTiles){
 			MTiles.cleanup();
 		}
@@ -570,17 +504,16 @@ protected:
 
 	void populateCommandBuffer(VkCommandBuffer commandBuffer, int currentImage)
 	{
-		// std::cout << "HI";
 		// binds the pipeline
-		// For a pipeline object, this command binds the corresponding pipeline to the command buffer passed in its parameter
 		P.bind(commandBuffer);
 
-		// binds the model
+		// ----------Start Splash----------
 		MSplash1.bind(commandBuffer);
 		DSSplash1.bind(commandBuffer, P, 0, currentImage);
 		vkCmdDrawIndexed(commandBuffer,
 									 static_cast<uint32_t>(MSplash1.indices.size()), 1, 0, 0, 0);
 
+		//----------End Splash----------
 		MSplash2.bind(commandBuffer);
 		DSSplash2.bind(commandBuffer, P, 0, currentImage);
 		vkCmdDrawIndexed(commandBuffer,
@@ -588,20 +521,6 @@ protected:
 
 		// binds the data set
 		DS1.bind(commandBuffer, P, 0, currentImage);
-		// For a Dataset object, this command binds the corresponding dataset
-		// to the command buffer and pipeline passed in its first and second parameters.
-		// The third parameter is the number of the set being bound
-		// As described in the Vulkan tutorial, a different dataset is required for each image in the swap chain.
-		// This is done automatically in file Starter.hpp, however the command here needs also the index
-		// of the current image in the swap chain, passed in its last parameter
-
-		// binds the model
-		// For a Model object, this command binds the corresponding index and vertex buffer
-		// to the command buffer passed in its parameter
-
-		// record the drawing command in the command buffer
-		// the second parameter is the number of indexes to be drawn. For a Model object,
-		// this can be retrieved with the .indices.size() method.
 
 		// ----------BlinnPhong Pipeline----------
 		PBlinn.bind(commandBuffer);
@@ -613,22 +532,20 @@ protected:
 
 		// Bind Dataset, Model and record drawing command in command buffer
 		for (int i = 0; i < NUM_OF_TILES / 2; i++) {
-			// Ocean
+			// ----------Ocean----------
 			MOcean[i].bind(commandBuffer);
 			DSOcean[i].bind(commandBuffer, PBlinn, 0, currentImage);
 			vkCmdDrawIndexed(commandBuffer,
 								static_cast<uint32_t>(MOcean[i].indices.size()), 1, 0, 0, 0);
-			// Sand
+			// ----------Sand----------
 			MSand[i].bind(commandBuffer);
 			DSSand[i].bind(commandBuffer, PBlinn, 0, currentImage);
 			vkCmdDrawIndexed(commandBuffer,
 												static_cast<uint32_t>(MSand[i].indices.size()), 1, 0, 0, 0);
 		}
 
-
-		// Bind Dataset, Model and record drawing command in command buffer
+		// ----------Obstacles----------
 		for (int i = 0; i < NUM_OF_TILES; i++){
-			// Obstacles
 			MObstacles[i].bind(commandBuffer);
 			DSObstacles[i].bind(commandBuffer, PBlinn, 0, currentImage);
 			vkCmdDrawIndexed(commandBuffer,
@@ -636,16 +553,15 @@ protected:
 
 		}
 
-		// Bind Dataset, Model and record drawing command in command buffer
+		// ----------Beachtiles----------
 		for (int i = 0; i < NUM_OF_TILES * 2; i++) {
-			// Beachtiles
 			MBeach[i].bind(commandBuffer);
 			DSBeach[i].bind(commandBuffer, PBlinn, 0, currentImage);
 			vkCmdDrawIndexed(commandBuffer,
 						static_cast<uint32_t>(MBeach[i].indices.size()), 1, 0, 0, 0);
 		}
 
-		// Skybox
+		// ----------Skybox----------
 		MSkybox.bind(commandBuffer);
 		DSSkybox.bind(commandBuffer, PBlinn, 0, currentImage);
 		vkCmdDrawIndexed(commandBuffer,
@@ -661,7 +577,6 @@ protected:
 			vkCmdDrawIndexed(commandBuffer,
 							 static_cast<uint32_t>(MTiles[i].indices.size()), 1, 0, 0, 0);
 		}
-
 
 		// ----------Score----------
 		score.populateCommandBuffer(commandBuffer, currentImage, scene.text);
@@ -682,17 +597,6 @@ protected:
 		glm::vec3 m = glm::vec3(0.0f), r = glm::vec3(0.0f);
 		bool fire = false;
 		getSixAxis(deltaT, m, r, fire);
-		// getSixAxis() is defined in Starter.hpp in the base class.
-		// It fills the float point variable passed in its first parameter with the time
-		// since the last call to the procedure.
-		// It fills vec3 in the second parameters, with three values in the -1,1 range corresponding
-		// to motion (with left stick of the gamepad, or ASWD + RF keys on the keyboard)
-		// It fills vec3 in the third parameters, with three values in the -1,1 range corresponding
-		// to motion (with right stick of the gamepad, or Arrow keys + QE keys on the keyboard, or mouse)
-		// If fills the last boolean variable with true if fire has been pressed:
-		//          SPACE on the keyboard, A or B button on the Gamepad, Right mouse button
-
-
 
 		// ----------GAME STATE----------
 		// gameState 0 -> START -> Press Space to start
@@ -717,7 +621,6 @@ protected:
 			DSSplash2.map(currentImage, &uboSplash2, sizeof(uboSplash2), 0);
 
 			initialized = 1;
-			//texts.pop_back();
 			scene.text = 0;
 
 			if (fire) {
@@ -775,13 +678,7 @@ protected:
 			}
 
 			// Change cameras when fire is pressed (between 0, 1 and 2)
-			if (fire) {
-				static int cam = 0;
-				cam = (cam + 1) % 3;
-			}
 
-			// Change cameras when fire is pressed (between 0, 1 and 2)
-			// just change when fire was pressed and it gets released
 			if (fire) {
 				prevFire = fire;
 			}
@@ -812,25 +709,12 @@ protected:
 				break;
 			}
 
-			// glm::vec3 camPos = camTarget + glm::vec3(0, 50, -80) / 2.0f; //debugging cam
-
-
-
 			View = glm::lookAt(camPos, camTarget, glm::vec3(0, 1, 0));
 
 			// translate car movement
 			World = glm::translate(glm::mat4(1), glm::vec3(-car.pos.x, 0, car.pos.z));
 			ubo2.mvpMat = Prj * View * World;
 			DS2.map(currentImage, &ubo2, sizeof(ubo2), 0);
-			// the .map() method of a DataSet object, requires the current image of the swap chain as first parameter
-			// the second parameter is the pointer to the C++ data structure to transfer to the GPU
-			// the third parameter is its size
-			// the fourth parameter is the location inside the descriptor set of this uniform block
-
-			// Init Gubo
-			// gubo.lightColor = glm::vec4(0.82f, 0.2f, 0.48f, 1.0f);
-			// gubo.lightDir = glm::normalize(glm::vec3(2.0f, 3.0f, -3.0f));
-			// gubo.spotLightDir = glm::normalize(glm::vec3(0, 0, -1.0f));
 
 			// Road Generation
 			// TILE SIZE 16 x 16
@@ -849,12 +733,10 @@ protected:
 				gubo.eyePos = camPos;
 				gubo.spotLightDir = glm::normalize(glm::vec3(0, 0, 1));
 				gubo.spotLightPos = glm::vec3(-(car.pos.x - 0.5f), car.pos.y, car.pos.z + 1.5f);
-				// gubo.spotLightPosRight = glm::vec3(-(car.pos.x + 1), car.pos.y, car.pos.z);
 				DSTiles[i].map(currentImage, &gubo, sizeof(gubo), 2);
-				// DSTiles[i].map(currentImage, &slo, sizeof(slo), 3); 
 			}
 
-			// Beach (8 x 8)
+			// Beach 
 			glm::mat4 World_Beach[NUM_OF_TILES * 2];
 			for (int i = 0; i < NUM_OF_TILES * 2; i++) {
 				if (beach[i].pos.z + 16 < car.pos.z) {
@@ -897,21 +779,18 @@ protected:
 				DSSand[i].map(currentImage, &ubo3, sizeof(ubo3), 0);
 			}
 
-
-
+			// Skybox
 			glm::mat4 World_Skybox;
 			World_Skybox = glm::translate(glm::mat4(1), glm::vec3(0, 35.0f, car.pos.z + 180)) * glm::scale(glm::mat4(1), glm::vec3(2.5f)) * glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0, 1, 0)) *
 				glm::rotate(glm::mat4(1.0f), glm::radians(90.0f), glm::vec3(1, 0, 0));
 			ubo3.mvpMat = Prj * View * World_Skybox;
 			DSSkybox.map(currentImage, &ubo3, sizeof(ubo3), 0);
 
-			// DS3.map(currentImage, &ubo3, sizeof(ubo3), 0);
 		}
 
 		if (scene.gameState == 2) {
 
-			// Camera
-			//Orthogonal projectiion for the splash
+			// Orthogonal projectiion for the splash
 
 			glm::mat4 Port = glm::scale(glm::mat4(1.0f), glm::vec3(1, -1, 1)) * glm::ortho(-Ar, Ar, -1.0f, 1.0f, 0.1f, 100.0f);
 			glm::mat4 View = glm::lookAt(glm::vec3(0, 0, 1), glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
@@ -927,6 +806,7 @@ protected:
 				prevFire = fire;
 			}
 
+			// Change to start screen
 			if (!fire && prevFire) {
 				scene.gameState = 0;
 				prevFire = fire;
